716407a516bf79daeec85bec148841c6a4ddc651
root <root@DESKTOP-276TNP6>
2025-02-04 17:32:32 +0100

    Update: new classifier model, dateparser

--- app/Main.py
+++ app/Main.py
@@ -1,23 +1,40 @@
 import streamlit as st
 from utils.preprocess import preprocess
-from utils.date_extraction import extract_dates
+from utils.date_extraction_small import extract_expiry_dates
 from utils.file_utils import save_uploaded_file, read_file_content
 from utils.pytesseract_extract import pytesseract_api_call
 import joblib
 import json
+import requests
 
 from dotenv import load_dotenv
 import os
+import numpy as np
 
 load_dotenv()
+
+BACKEND_URL = os.getenv("BACKEND_URL")
 
 # Load Model
 model = joblib.load(os.getenv("MODEL_PATH"))
 vectorizer = joblib.load(os.getenv("VECTORIZER_PATH"))
 
-os.makedirs("preds", exist_ok=True)
-os.makedirs("uploads", exist_ok=True)
-os.makedirs("extracted", exist_ok=True)
+def lr_predict(doc_text):
+    # Predict probabilities
+    probabilities = model.predict_proba(doc_text)
+
+    # Set confidence threshold
+    threshold = 0.62
+
+    predictions = []
+    for prob in probabilities:
+        max_prob = np.max(prob)
+        if max_prob < threshold:
+            predictions.append("Other")  # Low confidence → classify as Other
+        else:
+            predictions.append(model.classes_[np.argmax(prob)])  # Class A or B
+
+    return predictions
 
 st.markdown("# Main page 🎈")
 st.markdown("""
@@ -35,7 +52,29 @@ st.markdown("""
 
 uploaded_file = st.file_uploader("Upload a Document", type=["pdf"])
 
+headers = {
+    'accept': 'application/json',
+}
+
 if uploaded_file:
+    
+    # with st.spinner("Processing..."):
+    #     try:
+    #         files = {
+    #             'file': (uploaded_file.name, uploaded_file, 'application/pdf')
+    #         }
+            
+    #         # Send file to the FastAPI backend
+    #         response = requests.post(f"{BACKEND_URL}/api/v1/documents/upload", headers=headers, files=files)
+
+    #         if response.status_code == 200:
+    #             result = response.json()
+    #             st.success("PDF processed successfully!")
+    #         else:
+    #             st.error(f"Failed to process PDF: {response.status_code}")
+    #     except Exception as e:
+    #         st.error(f"Error: {e}")
+    
     file_path = save_uploaded_file(uploaded_file)
 
     with st.status("Processing... Please wait", expanded=True) as status:
@@ -46,10 +85,13 @@ if uploaded_file:
 
         processed_doc = preprocess(document_content)
         doc_vectorized = vectorizer.transform([processed_doc])
-        prediction = model.predict(doc_vectorized)[0]
-        expiring_date = extract_dates(document_content)
+        prediction = lr_predict(doc_vectorized)[0]
+        if prediction == "Other":
+            expiring_date = "None"
+        else:
+            expiring_date = extract_expiry_dates(document_content)
 
         with open(f'preds/{uploaded_file.name.replace(".pdf", ".json")}', "w") as json_file:
-            json.dump({'report_type': prediction, 'expiring_date': expiring_date}, json_file, indent=4)
+            json.dump({'report_type': prediction, 'expiring_date': str(expiring_date)}, json_file, indent=4)
 
     st.success("File uploaded successfully! Go to the pages to view the document.")
--- app/utils/file_utils.py
+++ app/utils/file_utils.py
@@ -1,16 +1,15 @@
 import os
+import shutil
 
-def save_uploaded_file(uploaded_file):
-    file_path = os.path.join("uploads", uploaded_file.name)
-    os.makedirs("uploads", exist_ok=True)
+def save_uploaded_file(uploaded_file, upload_dir="uploads"):
+    os.makedirs(upload_dir, exist_ok=True)
+    file_path = os.path.join(upload_dir, uploaded_file.name)
+    
     with open(file_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
+        shutil.copyfileobj(uploaded_file, f)
+    
     return file_path
 
 def read_file_content(file_path):
-    try:
-        with open(file_path, "r", encoding="utf-8") as f:
-            return f.read()
-    except UnicodeDecodeError:
-        with open(file_path, "r", encoding="latin-1") as f:
-            return f.read()
+    with open(file_path, "r", encoding="utf-8", errors="replace") as f:
+        return f.read()
